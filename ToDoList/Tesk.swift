8aa673c0a69a1b428d84f42a7b962bcd3ce4fe30
bocastle <bocastle1213@gmail.com>
2022-11-22 00:39:54 +0900

    스플래쉬 작업 및 깃 연결.

---
+++ ToDoList/Tesk.swift
@@ -0,0 +1,13 @@
+//
+//  Tesk.swift
+//  ToDoList
+//
+//  Created by bocastle on 2022/11/21.
+//
+
+import Foundation
+
+struct Task {
+    var title: String // 할일 내용 저장
+    var done: Bool // 할일이 완료 되었는지 여부 저장
+}
